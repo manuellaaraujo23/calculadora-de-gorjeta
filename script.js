6d2f3ffbe92dcb11f918784d4a86d9dca164e5e7
manuella <silva.manuella@eaportal.org>
2024-03-26 11:21:35 -0300

    sexto commit do JS E AGORA SIM PRROJETO FINALIZADO!

--- script.js
+++ script.js
@@ -86,3 +86,7 @@ const botaoLimpar=document.querySelector(".resultados button")
     document.querySelector(".gorjeta-total > strong").innerHTML= "R$ 0.00"
     document.querySelector(".total > strong").innerHTML= "R$ 0.00"
  }
+
+ conta=0
+ porcentagem=0
+ pessoas=0
